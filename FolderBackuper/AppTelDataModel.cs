dcdabfd1ff0ea2cf3711ecafb8b0e6c1513c66c4
SkyPowerGb <pyrobrunogb@gmail.com>
2024-11-01 18:35:48 +0100

    AppDataModel and basic desing added

---
+++ FolderBackuper/AppTelDataModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderBackuper
+{
+    internal class AppTelDataModel
+    {
+        public List<string> FoldersToBackup { get; set; }
+
+        public DateTime LastBackup { get; set; }
+
+        public string BackupsPath {  get; set; }
+
+
+
+
+    }
+}
--- FolderBackuper/Form1.Designer.cs
+++ FolderBackuper/Form1.Designer.cs
@@ -28,13 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbFoldersList = new System.Windows.Forms.ListBox();
+            this.tbBackupsPath = new System.Windows.Forms.TextBox();
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.btnSelectFolderPath = new System.Windows.Forms.Button();
+            this.btnAddFolderPath = new System.Windows.Forms.Button();
+            this.btnSelectBackupPath = new System.Windows.Forms.Button();
+            this.btnRemoveFolderPath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbFoldersList
+            // 
+            this.lbFoldersList.FormattingEnabled = true;
+            this.lbFoldersList.Location = new System.Drawing.Point(15, 93);
+            this.lbFoldersList.Name = "lbFoldersList";
+            this.lbFoldersList.Size = new System.Drawing.Size(277, 329);
+            this.lbFoldersList.TabIndex = 0;
+            // 
+            // tbBackupsPath
+            // 
+            this.tbBackupsPath.Location = new System.Drawing.Point(15, 525);
+            this.tbBackupsPath.Name = "tbBackupsPath";
+            this.tbBackupsPath.Size = new System.Drawing.Size(206, 20);
+            this.tbBackupsPath.TabIndex = 2;
+            // 
+            // tbFolderPath
+            // 
+            this.tbFolderPath.Location = new System.Drawing.Point(15, 43);
+            this.tbFolderPath.Name = "tbFolderPath";
+            this.tbFolderPath.Size = new System.Drawing.Size(206, 20);
+            this.tbFolderPath.TabIndex = 4;
+            // 
+            // btnSelectFolderPath
+            // 
+            this.btnSelectFolderPath.Location = new System.Drawing.Point(227, 43);
+            this.btnSelectFolderPath.Name = "btnSelectFolderPath";
+            this.btnSelectFolderPath.Size = new System.Drawing.Size(65, 23);
+            this.btnSelectFolderPath.TabIndex = 5;
+            this.btnSelectFolderPath.Text = "Odaberi";
+            this.btnSelectFolderPath.UseVisualStyleBackColor = true;
+            // 
+            // btnAddFolderPath
+            // 
+            this.btnAddFolderPath.Location = new System.Drawing.Point(298, 43);
+            this.btnAddFolderPath.Name = "btnAddFolderPath";
+            this.btnAddFolderPath.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFolderPath.TabIndex = 6;
+            this.btnAddFolderPath.Text = "Dodaj";
+            this.btnAddFolderPath.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectBackupPath
+            // 
+            this.btnSelectBackupPath.Location = new System.Drawing.Point(227, 525);
+            this.btnSelectBackupPath.Name = "btnSelectBackupPath";
+            this.btnSelectBackupPath.Size = new System.Drawing.Size(65, 20);
+            this.btnSelectBackupPath.TabIndex = 7;
+            this.btnSelectBackupPath.Text = "odaberi";
+            this.btnSelectBackupPath.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoveFolderPath
+            // 
+            this.btnRemoveFolderPath.Location = new System.Drawing.Point(298, 93);
+            this.btnRemoveFolderPath.Name = "btnRemoveFolderPath";
+            this.btnRemoveFolderPath.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveFolderPath.TabIndex = 8;
+            this.btnRemoveFolderPath.Text = "Makni";
+            this.btnRemoveFolderPath.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Popis datoteka ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Putanja do datoteke";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 509);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Lokacije za kopije";
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(298, 6);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(75, 23);
+            this.btnSettings.TabIndex = 12;
+            this.btnSettings.Text = "postavke";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(398, 557);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRemoveFolderPath);
+            this.Controls.Add(this.btnSelectBackupPath);
+            this.Controls.Add(this.btnAddFolderPath);
+            this.Controls.Add(this.btnSelectFolderPath);
+            this.Controls.Add(this.tbFolderPath);
+            this.Controls.Add(this.tbBackupsPath);
+            this.Controls.Add(this.lbFoldersList);
+            this.Name = "Form1";
+            this.Text = "DataBackup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbFoldersList;
+        private System.Windows.Forms.TextBox tbBackupsPath;
+        private System.Windows.Forms.TextBox tbFolderPath;
+        private System.Windows.Forms.Button btnSelectFolderPath;
+        private System.Windows.Forms.Button btnAddFolderPath;
+        private System.Windows.Forms.Button btnSelectBackupPath;
+        private System.Windows.Forms.Button btnRemoveFolderPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSettings;
     }
 }
 
